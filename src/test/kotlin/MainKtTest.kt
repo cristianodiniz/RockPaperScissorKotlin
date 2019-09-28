10d56ed071d95eec2907418629fdbed4ce40dfd2
Cristiano Diniz Pinto <cristiano.pinto@wibas-etarate.com>
2019-09-28 21:12:28 +0200

    #5 create matches (Add players,Track score, and Allow multiple rounds)

--- src/test/kotlin/MainKtTest.kt
+++ src/test/kotlin/MainKtTest.kt
@@ -1,6 +1,6 @@
-
-
-import enums.Options.*
+import enums.OptionsEnum
+import enums.OptionsEnum.*
+import enums.PlayerTypeEnum.*
 import exceptions.DrawException
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertThrows
@@ -8,6 +8,10 @@ import org.junit.jupiter.api.Test
 
 
 internal class MainKtTest {
+
+    private val player1 = Player("P1", HUMAN)
+    private val player2 = Player("P2", MACHINE)
+    private val matches = Matches(player1, player2)
 
     @Test
     fun the_application_should_run() {
@@ -18,36 +22,59 @@ internal class MainKtTest {
     @Test
     fun should_return_paper() {
         val expected = PAPER
-        var result = RockPaperScissor(ROCK, PAPER)
+        var result = matches.rockPaperScissor(ROCK, PAPER)
         assertEquals(expected, result)
-        result = RockPaperScissor(PAPER, ROCK)
+        result = matches.rockPaperScissor(PAPER, ROCK)
         assertEquals(expected, result)
     }
 
     @Test
     fun should_return_rock() {
         val expected = ROCK
-        var result = RockPaperScissor(ROCK, SCISSORS)
+        var result = matches.rockPaperScissor(ROCK, SCISSORS)
         assertEquals(expected, result)
-        result = RockPaperScissor(SCISSORS, ROCK)
+        result = matches.rockPaperScissor(SCISSORS, ROCK)
         assertEquals(expected, result)
     }
 
     @Test
     fun should_return_scissor() {
         val expected = SCISSORS
-        var result = RockPaperScissor(PAPER, SCISSORS)
+        var result = matches.rockPaperScissor(PAPER, SCISSORS)
         assertEquals(expected, result)
-        result = RockPaperScissor(SCISSORS, PAPER)
+        result = matches.rockPaperScissor(SCISSORS, PAPER)
         assertEquals(expected, result)
     }
 
     @Test
     fun should_return_draw_exception() {
         assertThrows(DrawException::class.java) {
-            RockPaperScissor(SCISSORS,SCISSORS)
+            matches.rockPaperScissor(SCISSORS, SCISSORS)
         }
     }
 
+    @Test
+    fun should_generate_at_less_one_option_of_each() {
+
+        val list = ArrayList<OptionsEnum>()
+        for (i in 1..1000) {
+            list.add(matches.randomOption())
+        }
+
+        assert(list.filter { it == ROCK }.size >= 0)
+        assert(list.filter { it == PAPER }.size >= 0)
+        assert(list.filter { it == SCISSORS }.size >= 0)
+
+    }
+
+    @Test
+    fun should_play_have_one_winner_or_draw(){
+
+        val matches = Matches(player1,player2)
+        for (i in 1..501) {
+            matches.autoPlay()
+        }
+        assert(matches.scoreBoard.scoreP1 != matches.scoreBoard.scoreP2)
+    }
 
 }
