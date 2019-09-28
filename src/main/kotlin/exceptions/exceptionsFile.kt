5976d3eda5ca6568c6f31dc323f5d0753a21fac6
Cristiano Diniz Pinto <cristiano.pinto@wibas-etarate.com>
2019-09-28 20:37:54 +0200

    #3 Create Logic for Playing Rock, Paper, Scissors

---
+++ src/main/kotlin/exceptions/exceptionsFile.kt
@@ -0,0 +1,5 @@
+package exceptions
+
+import java.lang.Exception
+
+class DrawException() : Exception("It is a Draw")
--- src/test/kotlin/MainKtTest.kt
+++ src/test/kotlin/MainKtTest.kt
@@ -1,11 +1,53 @@
 
 
-import org.junit.Test
+import enums.Options.*
+import exceptions.DrawException
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertThrows
+import org.junit.jupiter.api.Test
 
-internal class MainKtTest{
+
+internal class MainKtTest {
 
     @Test
     fun the_application_should_run() {
         main(emptyArray());
     }
+
+
+    @Test
+    fun should_return_paper() {
+        val expected = PAPER
+        var result = RockPaperScissor(ROCK, PAPER)
+        assertEquals(expected, result)
+        result = RockPaperScissor(PAPER, ROCK)
+        assertEquals(expected, result)
+    }
+
+    @Test
+    fun should_return_rock() {
+        val expected = ROCK
+        var result = RockPaperScissor(ROCK, SCISSORS)
+        assertEquals(expected, result)
+        result = RockPaperScissor(SCISSORS, ROCK)
+        assertEquals(expected, result)
+    }
+
+    @Test
+    fun should_return_scissor() {
+        val expected = SCISSORS
+        var result = RockPaperScissor(PAPER, SCISSORS)
+        assertEquals(expected, result)
+        result = RockPaperScissor(SCISSORS, PAPER)
+        assertEquals(expected, result)
+    }
+
+    @Test
+    fun should_return_draw_exception() {
+        assertThrows(DrawException::class.java) {
+            RockPaperScissor(SCISSORS,SCISSORS)
+        }
+    }
+
+
 }
